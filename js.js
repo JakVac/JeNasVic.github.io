d77dfda0110f2e538b108b969a42caf8b2d52de4
JakVac <JakVac@users.noreply.github.com>
2019-02-23 15:49:07 +0100

    new

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
